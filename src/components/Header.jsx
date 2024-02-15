339178380d27a7a6153c7698e2c7dd7a2fb31698
BahkoHatake <cbranko01@gmail.com>
2024-02-16 00:58:58 +0100

    add toggle dark mode

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,16 @@
 import icon from "../assets//moon-icon-512x512-fm9crgpt.png";
 
 export default function Header() {
+  function toggleDarkTheme() {
+    document.body.classList.toggle("dark-theme");
+  }
+
   return (
     <header>
       <h1>Were in the world?</h1>
       <div>
         <img src={icon} alt="" />
-        <button>Dark Mode</button>
+        <button onClick={toggleDarkTheme}>Dark Mode</button>
       </div>
     </header>
   );
