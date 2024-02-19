dfe58db10f08e9da9f468a7225ba91cd1b6ffa51
BahkoHatake <cbranko01@gmail.com>
2024-02-19 19:20:07 +0100

    Ready for deployment

--- src/App.jsx
+++ src/App.jsx
@@ -4,22 +4,25 @@ import RootPage from "./pages/Root";
 import Main from "./pages/Main";
 import CountrieInfo from "./pages/CountrieInfo";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <RootPage />,
-    children: [
-      {
-        path: "/",
-        element: <Main />,
-      },
-      {
-        path: "/:countrieId",
-        element: <CountrieInfo />,
-      },
-    ],
-  },
-]);
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <RootPage />,
+      children: [
+        {
+          path: "/",
+          element: <Main />,
+        },
+        {
+          path: "/:countrieId",
+          element: <CountrieInfo />,
+        },
+      ],
+    },
+  ],
+  { basename: import.meta.env.DEV ? "/" : "/react-vite-gh-pages/" }
+);
 
 function App() {
   return (
